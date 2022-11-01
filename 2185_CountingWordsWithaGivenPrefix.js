62aa12fc3f52d7bb2a8bf17948ee7f35df93dbd2
xndcmd <xndcmd@gmail.com>
2022-11-01 16:27:30 -0700

    2185. Counting Words With a Given Prefix

---
+++ 2185_CountingWordsWithaGivenPrefix.js
@@ -0,0 +1,19 @@
+// You are given an array of strings words and a string pref.
+// Return the number of strings in words that contain pref as a prefix.
+// A prefix of a string s is any leading contiguous substring of s.
+
+// traverse the list, then check each word is with a given prefix (pref)
+
+// words = ["pay","attention","practice","attend"];
+// pref = "at";
+
+const prefixCount = (words, pref) => {
+
+    let count = 0;
+
+    for (const word of words) {
+        word.startsWith(pref) && ++count;
+    };
+
+    return count;
+};
