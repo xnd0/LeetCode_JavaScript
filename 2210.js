ca96b57bd8ae575b34576bd252aed4d0b89cfc19
xndcmd <xndcmd@gmail.com>
2022-11-12 11:18:13 -0800

    2210. Count Hills and Valleys in an Array

---
+++ 2210.js
@@ -0,0 +1,60 @@
+// 2210. Count Hills and Valleys in an Array
+
+// You are given a 0-indexed integer array nums. An index i is part of a hill in nums if the closest non-equal neighbors of i are smaller than nums[i]. Similarly, an index i is part of a valley in nums if the closest non-equal neighbors of i are larger than nums[i]. Adjacent indices i and j are part of the same hill or valley if nums[i] == nums[j].
+
+// Note that for an index to be part of a hill or valley, it must have a non-equal neighbor on both the left and right of the index.
+
+// Return the number of hills and valleys in nums.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+let nums = [2, 4, 1, 1, 6, 5];
+
+var countHillValley = function (nums) {
+
+    let total = 0;
+    let hill = 0;
+    let valley = 0;
+
+    let list = [];
+
+    // check for, and remove non-equal neighbor element(s). Creates new list array.
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] !== nums[i - 1]) {
+            list.push(nums[i])
+        };
+    };
+
+    // check for local min/max and increment
+    for (let i = 1; i < nums.length - 1; i++) {
+
+        if (list[i] > list[i - 1] && list[i] > list[i + 1]) {
+            hill++;
+        };
+
+        if (list[i] < list[i - 1] && list[i] < list[i + 1]) {
+            valley++;
+        };
+    };
+
+    total = (hill + valley);
+
+    return total;
+};
+
+console.log(countHillValley(nums));
+
+// Input: nums = [2,4,1,1,6,5]
+// Output: 3
+// Explanation:
+// At index 0: There is no non-equal neighbor of 2 on the left, so index 0 is neither a hill nor a valley.
+// At index 1: The closest non-equal neighbors of 4 are 2 and 1. Since 4 > 2 and 4 > 1, index 1 is a hill. 
+// At index 2: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 2 is a valley.
+// At index 3: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 3 is a valley, but note that it is part of the same valley as index 2.
+// At index 4: The closest non-equal neighbors of 6 are 1 and 5. Since 6 > 1 and 6 > 5, index 4 is a hill.
+// At index 5: There is no non-equal neighbor of 5 on the right, so index 5 is neither a hill nor a valley. 
+// There are 3 hills and valleys so we return 3.
